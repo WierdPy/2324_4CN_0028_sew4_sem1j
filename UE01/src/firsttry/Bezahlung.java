9c76f0d8c8da1d961ad921394031fae5b4e7ce29
Burhan Özbek <burhanozbek89@gmail.com>
2023-10-08 14:58:56 +0200

    1.3 IDK

--- UE01/src/firsttry/Bezahlung.java
+++ UE01/src/firsttry/Bezahlung.java
@@ -1,4 +1,5 @@
 package firsttry;
+import java.util.regex.*;
 
 public class Bezahlung {
     public static void main(String[] args) {
@@ -9,5 +10,120 @@ public class Bezahlung {
         String[] words = a.replaceAll("<[^>]*>","").split("\\s+"); // Remove HTML tags and split the string into words
         System.out.println(words.length); // Print the count of words
         return words.length; // Return the count of words
+    }
+
+    public static int todo(String a){
+
+
+        int wordCounter = 0; // Initialize the word counter
+        int htmltagflag = 0; // Initialize the HTML tag counter
+        boolean stringflag = false; // Initialize the string flag
+        boolean wordflag = false; // Initialize the word flag
+
+        for (int i = 0; i < a.length(); i++) {
+            String currentChar = a.charAt(i) + "";
+
+            if (!currentChar.matches("\\\\")) {
+                if (currentChar.matches("\\w") && !wordflag && htmltagflag == 0) {
+                    wordflag = true;
+                    wordCounter++;
+                } else if (currentChar.matches("[\\s:.\"]") && htmltagflag == 0) {
+                    wordflag = false;
+                } else if (currentChar.matches("<") && !stringflag) {
+                    wordflag = false;
+                    htmltagflag++;
+                } else if (currentChar.matches(">") && !stringflag) {
+                    wordflag = false;
+                    htmltagflag--;
+                } else if (currentChar.matches("\"") && htmltagflag > 0) {
+                    wordflag = false;
+                    stringflag = !stringflag;
+                }
+            }
+        }
+        return wordCounter;
+
+        /*
+        System.out.println();
+        System.out.println("ÜBUNG BEGINNT");
+
+        //crate a regex pattern to find all "
+
+        Character regex = '"';
+        Pattern pattern = Pattern.compile(Character.toString(regex));
+        Matcher matcher = pattern.matcher(a);
+        int countofbigger = 0;
+        while (matcher.find()) {
+            countofbigger++;
+        }
+        //print countofbigger
+        boolean couttobiggeradd = countofbigger % 2 != 0;
+        System.out.print("countofbigger: ");
+        System.out.print(countofbigger);
+        System.out.println();
+
+
+        //assertEquals(4, todo(" \\\"null\\\" eins<img alt=\"<bild \\\" keinwort keinwort\" keinwort>zwei \"drei\""));
+        int htmltagcount = 0;
+        int wordnum = 0;
+        boolean isword = false;
+        boolean isblocked = false;
+        for (int i = 0; i < a.length(); i++) {
+            //if a character a lette
+            if (a.charAt(i) == '"'){
+                if (isblocked == false){
+                    if (couttobiggeradd != true && countofbigger != 1){
+                    isblocked = true;
+                    countofbigger--;
+                    System.out.println("isblocked" + i);
+                    }
+                }
+                else{
+                        isblocked = false;
+                        countofbigger--;
+                        System.out.println("unblocked" + i);
+                }
+            }
+            //write me a if statement that looks if the countofbigger is odd or even then if it is add when countofbigger is 1 then isblocked = false;
+            if (couttobiggeradd == true && countofbigger == 1){
+                isblocked = false;
+                System.out.println("unblocked" + i + "BUT COMBO");
+            }
+            if (isblocked == false || countofbigger == 1) {
+                if (Character.isLetter(a.charAt(i))) {
+                    if (htmltagcount == 0) {
+                        if (isword == false) {
+                            wordnum++;
+                            System.out.println(a.charAt(i));
+                            isword = true;
+                        }
+                    }
+                } else {
+                    isword = false;
+                }
+                if (a.charAt(i) == '<' && htmltagcount < 1) {
+
+                    htmltagcount++;
+                    System.out.println(i);
+                    //}
+                    isword = false;
+                }
+                if (a.charAt(i) == '>' && htmltagcount > 0) {
+                    System.out.println(i);
+                    htmltagcount--;
+                    isword = false;
+                }
+            }
+        }
+        System.out.print("htmltagcount: ");
+        System.out.print(htmltagcount);
+        System.out.println();
+        System.out.println(isword);
+        System.out.println("ÜBUNG ZU ENDE");
+
+        return wordnum;
+
+
+         */
     }
 }
--- UE01/src/firsttry/BezahlungTest.java
+++ UE01/src/firsttry/BezahlungTest.java
@@ -3,22 +3,136 @@ package firsttry;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static firsttry.Bezahlung.count;
+import static firsttry.Bezahlung.todo;
 
 public class BezahlungTest {
 
+
     @Test
-    public void testCount() {
+    public void leicht() {
         // Test case 1: Check if count method returns 1 for input "eins"
-        assertEquals(1, count("eins "));
+        assertEquals(1, todo("eins "));
 
         // Test case 2: Check if count method returns 1 for input "eins "
-        assertEquals(1, count("eins "));
+        assertEquals(1, todo("eins "));
 
         // Test case 3: Check if count method returns 3 for input "ein erster Text"
-        assertEquals(3, count("ein erster Text"));
+        assertEquals(3, todo("ein erster Text"));
 
-        // Test case 4: Ignore HTML tags
-        assertEquals(2, count(" eins <html>"));// ==> 1
 
     }
+
+
+
+    @Test
+    public void testEmptyString() {
+        assertEquals(0, todo(""));
+    }
+
+    @Test
+    public void testWhitespaceString() {
+        assertEquals(0, todo(" "));
+        assertEquals(0, todo("  "));
+    }
+
+    @Test
+    public void testSingleWord() {
+        assertEquals(1, todo("eins"));
+        assertEquals(1, todo(" eins"));
+        assertEquals(1, todo("eins "));
+        assertEquals(1, todo(" eins "));
+        assertEquals(1, todo(" eins  "));
+        assertEquals(1, todo("  eins "));
+        assertEquals(1, todo("  eins  "));
+    }
+
+    @Test
+    public void testSingleWordWithPunctuation() {
+        assertEquals(1, todo("eins:"));
+        assertEquals(1, todo(":eins"));
+        assertEquals(1, todo(":eins:"));
+        assertEquals(1, todo(" eins  "));
+        assertEquals(1, todo(" eins : "));
+        assertEquals(1, todo(": eins :"));
+        assertEquals(3, todo("ein erster Text"));
+        assertEquals(3, todo(" ein  erster   Text      "));
+        assertEquals(3, todo("ein:erster.Text"));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+        assertEquals(1, todo("a"));
+        assertEquals(1, todo(" a"));
+        assertEquals(1, todo("a "));
+        assertEquals(1, todo(" a "));
+    }
+
+    @Test
+    public void testWithHtmlTags() {
+        assertEquals(1, todo(" eins  <html> "));
+
+        assertEquals(1, todo(" eins  < html> "));
+        assertEquals(1, todo(" eins  <html > "));
+        assertEquals(1, todo(" eins  < html > "));
+        assertEquals(4, todo(" eins <html> zwei<html>drei <html> vier"));
+        assertEquals(2, todo(" eins <html> zwei "));
+        assertEquals(2, todo(" eins <html>zwei "));
+        assertEquals(2, todo(" eins<html> zwei "));
+        assertEquals(2, todo(" eins<html>zwei "));
+        assertEquals(2, todo(" eins<img alt=\"xxx\" > zwei"));
+        assertEquals(2, todo(" eins<img alt=\"xxx yyy\" > zwei"));
+
+
+    }
+
+    @Test
+    public void testWithQuotedText() {
+        assertEquals(2, todo(" eins \"zwei\" "));
+        assertEquals(2, todo(" eins\"zwei\" "));
+        assertEquals(2, todo(" eins \"zwei\""));
+        assertEquals(3, todo(" eins \"zwei\"drei"));
+        assertEquals(3, todo(" eins \"zwei\" drei"));
+    }
+
+    @Test
+    public void testTrickyHtml() {
+        //assertEquals(1, todo(" eins<html"));
+
+        //assertEquals(2, todo(" eins<img alt=\"<bild>\" > zwei"));
+
+        //assertEquals(2, todo(" eins<img alt=\"bild>\" > zwei"));
+
+
+        //assertEquals(2, todo(" eins<img alt=\"<bild>\" keinwort> zwei"));
+
+        //assertEquals(2, todo(" eins<img alt=\"<bild>\" src=\"bild.png\" >zwei"));
+
+        assertEquals(2, todo(" eins<img alt=\"<bild\" keinwort>zwei"));
+
+        assertEquals(1, todo(" eins<img alt=\"<bild\" keinwort"));
+        assertEquals(2, todo(" eins<img alt=\"<bild\" keinwort> zwei"));
+        assertEquals(1, todo(" eins<img alt=\"<bild keinwort> keinwort"));
+
+        assertEquals(2, todo(" eins<img alt=\"<bild keinwort keinwort\">zwei"));
+        assertEquals(2, todo(" eins<img alt=\"<bild keinwort< keinwort\">zwei"));
+
+
+
+
+    }
+
+
+    @Test
+    public void testEscapingCharacters() {
+        assertEquals(2, todo(" eins<img alt=\"<bild \\\" keinwort> keinwort\" keinwort>zwei"));
+        assertEquals(2, todo(" eins<img alt=\"<bild \\\" keinwort<keinwort\" keinwort>zwei"));
+        assertEquals(2, todo(" eins<img alt=\"<bild \\\" keinwort keinwort\" keinwort>zwei"));
+        assertEquals(4, todo(" \\\"null\\\" eins<img alt=\"<bild \\\" keinwort keinwort\" keinwort>zwei \"drei\""));
+    }
+
+
+
+
+
+
 }
